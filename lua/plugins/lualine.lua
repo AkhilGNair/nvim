832a6ab62b3119e141f6ac6ab60d19f3483d9e70
Akhil Nair <akhil.nair@virginmediao2.co.uk>
2024-02-09 17:15:11 +0000

    venv lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,32 +1,34 @@
 return {
-	{
-		"nvim-lualine/lualine.nvim",
-		event = "VeryLazy",
-		config = function()
-			local function truncate_branch_name(branch)
-				if not branch or branch == "" then
-					return ""
-        else
-          return branch
-				end
-			end
-
-			require("lualine").setup({
-				options = {
-					theme = "catppuccin",
-					globalstatus = true,
-					component_separators = { left = "", right = "" },
-					section_separators = { left = "█", right = "█" },
-				},
-				sections = {
-					lualine_c = {
-						{ "filename", path = 1 },
-					},
-					lualine_x = {
-						"filetype",
-					},
-				},
-			})
-		end,
-	},
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "catppuccin",
+          globalstatus = true,
+          component_separators = { left = "", right = "" },
+          section_separators = { left = "█", right = "█" },
+        },
+        sections = {
+          lualine_c = { { "filename", path = 1 } },
+          lualine_x = { "filetype" },
+          lualine_z = {
+            {
+              function()
+                if vim.bo.filetype == 'python' then
+                  return require('lualine.components.venv-selector').update_status()
+                else
+                  return ''
+                end
+              end,
+              icon = ' ',
+            },
+          },
+        },
+      })
+    end,
+  },
 }
+
+-- 'venv-selector', icon = ' '
